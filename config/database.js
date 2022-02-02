16f2724869bf4953fde44a45a76df2a6ae7a947f
Sajjad Haider <sajjad.haider.1@gmail.com>
2022-02-02 12:58:26 +0500

    d pg connection again

--- config/database.js
+++ config/database.js
@@ -1,15 +1,18 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+const parse = require('pg-connection-string').parse;
+    const config = parse(process.env.DATABASE_URL);
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false
+          },
+        },
+        debug: false,
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+    });
